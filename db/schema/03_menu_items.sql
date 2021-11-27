bf3cd169ad41312da7563f6c3e18e5efe60905ba
Anastassia <nastiatsv@gmail.com>
2021-11-27 11:08:38 -0800

    created tables for database

---
+++ db/schema/03_menu_items.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS menu_items CASCADE;
+
+CREATE TABLE menu_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  price_in_cents INTEGER NOT NULL DEFAULT 0,
+  picture_url VARCHAR255 NOT NULL
+
+);
