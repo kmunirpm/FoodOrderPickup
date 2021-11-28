c6eb14f61483458e0f350cd829d556453f672ce3
kmunirpm <km092009@gmail.com>
2021-11-28 22:33:56 +0000

    seed files created

---
+++ db/seeds/2_menu_items.sql
@@ -0,0 +1,7 @@
+INSERT INTO menu_items (id, name, description, price_in_cents, picture_url) 
+VALUES ('Stuffed Flatbread', 'some description about Stuffed Flatbread', '1000', '/images/menu1.png')
+('2', 'Poké Bowl', 'some description about Poké Bowl', '700', '/images/menu2.png')
+('3', 'Aburi', 'some description about Aburi', '900', '/images/menu3.png')
+('4', 'Pizza', 'some description about Pizza', '1500', '/images/menu4.jpg')
+('5', 'Burger', 'Some description about burgers', '300', '/images/menu5.jpg')
+('6', 'Rolls', 'Some description about rolls', '300', '/images/menu6.jpeg');
