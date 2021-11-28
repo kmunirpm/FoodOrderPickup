c10b4f2ce6e06ad25cc24ee2d21dbe7c42f222fb
Morsal Niyaz <morsalniyaz@gmail.com>
2021-11-28 15:43:48 -0800

    Made Page Scrollable and Fixed Columns

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice', 'alice@gmail.com', 12345678);
-INSERT INTO users (name) VALUES ('Kira', 'kira@gmail.com', 12345678);
+INSERT INTO users (name, email, phone) VALUES ('Alice', 'alice@gmail.com', 12345678),
+('Kira', 'kira@gmail.com', 12345678);
