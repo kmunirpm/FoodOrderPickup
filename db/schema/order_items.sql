0090efb36d0903306a9cb3e71ee39c30301b67cd
kmunirpm <km092009@gmail.com>
2021-11-28 22:09:13 +0000

    tables created

---
+++ db/schema/order_items.sql
@@ -0,0 +1,10 @@
+-- Drop and recreate ordered_items table (Example)
+
+DROP TABLE IF EXISTS ordered_items CASCADE;
+
+CREATE TABLE ordered_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+  menu_item_id INTEGER REFERENCES menu_items(id) ON DELETE CASCADE,
+  quantity SMALLINT NOT NULL
+);
